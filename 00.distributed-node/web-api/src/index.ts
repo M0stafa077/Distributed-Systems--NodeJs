f184f93485d34598886415ef55686b67b4d1b049
Mostafa Asaad <ma4190243@gmail.com>
2025-02-02 20:11:10 +0200

    feat: Add HAProxy for load balancing and health check endpoints for web APIs

--- 00.distributed-node/web-api/src/index.ts
+++ 00.distributed-node/web-api/src/index.ts
@@ -32,7 +32,12 @@ app.get("/", async (req, reply) => {
     }
 });
 
-app.listen({ port: 3001, host: "0.0.0.0" }, (err, address) => {
+app.get("/health", async (req, reply) => {
+    console.log("Health check");
+    return "Ok";
+});
+
+app.listen({ port: Number(process.env.PORT || 3001), host: "0.0.0.0" }, (err, address) => {
     app.log.info(`server listening on ${address}`);
 });
 
